360ebfb1f2dbb78708bf0da864d83e60e10036fc
Raul Anton <raulantonechevarria@gmail.com>
2024-12-06 10:33:55 +0100

    Better format

--- src/raids.py
+++ src/raids.py
@@ -1,17 +1,17 @@
 import json
 
-with open('src/raids2.json', 'r', encoding='utf-8') as file:
+with open('src/raids.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-matching_raids = [
-    raid for raid in data.get("raids", [])
-    if raid.get("pokemon_id") == 854 and (raid.get("form") == 2478 or raid.get("form") == 2481)
-]
+matching_raids = []
+for raid in data.get("raids", []):
+    if raid.get("pokemon_id") == 854 and raid.get("form") in [2478, 2481]:
+        matching_raids.append(raid)
 
 if matching_raids:
-    print("Raids con pokemon_id igual a 854 y form diferente de 2477:")
+    print("Raids con pokemon_id igual a 854 y form igual a 2478 o 2481:")
     for raid in matching_raids:
         print(raid)
-        print()
+        print() 
 else:
-    print("No se encontró ningún raid con pokemon_id igual a 854 y form diferente de 2477.")
+    print("No se encontró ningún raid con pokemon_id igual a 854 y form igual a 2478 o 2481.")
