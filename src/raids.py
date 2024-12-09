b54abf1d3251856d8f23b5594230663c3591fc60
Raul Anton <raulantonechevarria@gmail.com>
2024-12-09 22:28:45 +0100

    better distribution

--- src/raids.py
+++ src/raids.py
@@ -14,23 +14,26 @@ def fetch_json(link):
     else:
         print(f"Error: {response.status_code}")
         return None
+    
+pokemon_lf = 854
+forms_lf = [2478, 2481]
 
 json_nyc = fetch_json("https://nycpokemap.com/raids.php?time=")
 json_sing = fetch_json("https://sgpokemap.com/raids.php?time=")
 
 matching_raids = []
 for raid in json_nyc.get("raids", []):
-    if raid.get("pokemon_id") == 854 and raid.get("form") in [2478, 2481]:
+    if raid.get("pokemon_id") == pokemon_lf and raid.get("form") in forms_lf:
         matching_raids.append(raid)
 
 for raid in json_sing.get("raids", []):
-    if raid.get("pokemon_id") == 854 and raid.get("form") in [2478, 2481]:
+    if raid.get("pokemon_id") == pokemon_lf and raid.get("form") in forms_lf:
         matching_raids.append(raid)
 
 if matching_raids:
-    print("Raids con pokemon_id igual a 854 y form igual a 2478 o 2481:")
+    print(f"Raids con pokemon_id igual a {pokemon_lf} y form {forms_lf}:")
     for raid in matching_raids:
         print(raid)
         print() 
 else:
-    print("No se encontró ningún raid con pokemon_id igual a 854 y form igual a 2478 o 2481.")
+    print(f"No se encontraron raids con pokemon_id igual a {pokemon_lf} y form {forms_lf}:")
